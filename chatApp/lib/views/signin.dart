158c678591918131757d90b90254cfa81d27cfc4
Tharindu97 <tharindukavishna@gmail.com>
2020-12-03 10:12:07 +0530

    sign in ui page completed

---
+++ chatApp/lib/views/signin.dart
@@ -0,0 +1,103 @@
+import 'package:chatApp/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 50,
+          alignment: Alignment.bottomRight,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  style: simpleTextFieldStyle(),
+                  decoration: textFieldInputDecoration('email'),
+                ),
+                TextField(
+                  style: simpleTextFieldStyle(),
+                  decoration: textFieldInputDecoration('password'),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: Text(
+                      'Forgot Password?',
+                      style: simpleTextFieldStyle(),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        const Color(0XFF007EF4),
+                        const Color(0XFF2A75BC),
+                      ]),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text('Sign In', style: mediumTextStyle()),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text(
+                    'Sign In with Google',
+                    style: TextStyle(color: Colors.black87, fontSize: 17),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have account? ",
+                      style: mediumTextStyle(),
+                    ),
+                    Text(
+                      "Register now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          decoration: TextDecoration.underline),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
